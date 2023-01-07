6b6e9f17cb450f6efc2d04167779b1760c76921d
Arunabh Das <arunabhdas@gmail.com>
2023-01-07 17:49:47 -0500

    Added summarization.py

---
+++ nextmachina/finetune.py
@@ -0,0 +1,14 @@
+from transformers import Trainer, TrainingArguments
+
+training_args = TrainingArguments("test-trainer")
+
+trainer = Trainer(
+    model,
+    training_args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+)
+
+trainer.train()
